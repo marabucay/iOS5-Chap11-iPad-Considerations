0c4f1af69d6623964f7e65dfe130a767668383fc
=Shaliemar Abucay <shlabucay@gmail.com>
2013-09-18 10:47:16 +0800

    Created Presidents Project on iPad Platform.

---
+++ Presidents/Presidents/BIDDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  BIDDetailViewController.h
+//  Presidents
+//
+//  Created by Maria Alice C. Lim on 9/18/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDDetailViewController : UIViewController <UISplitViewControllerDelegate>
+
+@property (strong, nonatomic) id detailItem;
+
+@property (strong, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
---
+++ Presidents/Presidents/BIDMasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  BIDMasterViewController.h
+//  Presidents
+//
+//  Created by Maria Alice C. Lim on 9/18/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class BIDDetailViewController;
+
+@interface BIDMasterViewController : UITableViewController
+
+@property (strong, nonatomic) BIDDetailViewController *detailViewController;
+
+@end
